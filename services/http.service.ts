0ae1d3253792c943fab6577be2a660e359cd9db5
avimi13 <alfredorequenez57libra@gmail.com>
2022-11-18 13:26:36 -0600

    Modificaciones en request add new endPoint

--- services/http.service.ts
+++ services/http.service.ts
@@ -1,4 +1,4 @@
-import { ListCategoriesResponse } from '../interfaces/index';
+import { ListRecipesResponse } from '../interfaces/index';
 
 const baseUrl = 'http://localhost:3000';
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjAxR0gySkpBSjVBSFo2OUszRlI1NFBHQkswIiwiaWF0IjoxNjY3NjE0MjMwLCJleHAiOjIyNjc2MTQyMzB9.AtiOxxHse5cL1MLpi_jjF6FEjg_ofP6a02nss37HBBM";
@@ -15,8 +15,41 @@ export class HttpClient {
                 Authorization: `Bearer ${token}`
             }
         });
-        const data: ListCategoriesResponse = await response.json();
+        const data: ListRecipesResponse = await response.json();
 
         return data;
+    }
+    async post(endPoint = "recipes") {
+        const response = await fetch(`${baseUrl}/${endPoint}`, {
+            headers: {
+                Autorization: `Bearer ${token}`
+            }
+        })
+        const store: ListRecipesResponse = await response.json();
+        return store;
+
+    }
+    async getOne(endPoint = "recipes") {
+        const response = await fetch(`${baseUrl}/:id${endPoint}`, {
+            headers: {
+                Autorization: `Bearer ${token}`
+            }
+        })
+        const store: ListRecipesResponse = await response.json();
+        return store;
+    }
+
+    async delete(endPoint = "recipes") {
+        try {
+            const response = await fetch(`${baseUrl}/:id${endPoint}`, {
+                headers: {
+                    Autorizatio: `Bearer ${token}`
+                }
+            })
+            const store: ListRecipesResponse = await response.json();
+            return store;
+        } catch (error: any) {
+            console.log(error)
+        }
     }
 }
